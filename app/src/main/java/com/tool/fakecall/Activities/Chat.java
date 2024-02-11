40ca8e35abe8291c5c4c6f6a5af5ff31a1baff5f
mac <mac@macs-mbp-2.home>
2024-02-11 20:42:40 +0500

    Initial commit

---
+++ app/src/main/java/com/tool/fakecall/Activities/Chat.java
@@ -0,0 +1,16 @@
+package com.tool.fakecall.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tool.fakecall.R;
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
---
+++ app/src/main/java/com/tool/fakecall/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tool.fakecall.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tool.fakecall.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
